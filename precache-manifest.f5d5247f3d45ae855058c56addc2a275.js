136647226d76507fec12caa73940fb6bf48b6d8f
SChoi <pill21361@gmail.com>
2021-02-23 17:48:49 +0000

    Updates

---
+++ precache-manifest.f5d5247f3d45ae855058c56addc2a275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "777c49b0ba5ac0fe03ef",
+    "url": "/movie_clone_coding/static/js/runtime~main.52cfa36b.js"
+  },
+  {
+    "revision": "4186b7156fe2bc64acd2",
+    "url": "/movie_clone_coding/static/js/main.2538e241.chunk.js"
+  },
+  {
+    "revision": "bee7d8cd15b03ee289fb",
+    "url": "/movie_clone_coding/static/js/2.8e36e3e3.chunk.js"
+  },
+  {
+    "revision": "4186b7156fe2bc64acd2",
+    "url": "/movie_clone_coding/static/css/main.d945ce14.chunk.css"
+  },
+  {
+    "revision": "58abd6a52b5ca11ecf1054d23b1074dc",
+    "url": "/movie_clone_coding/index.html"
+  }
+];
